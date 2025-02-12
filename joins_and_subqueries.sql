00a89ec3e02fafdc0ae11dbbe269f50262c15d3e
theHinneh <thehinneh1@gmail.com>
2025-02-12 00:40:12 -0500

    modifying data

--- joins_and_subqueries.sql
+++ joins_and_subqueries.sql
@@ -55,3 +55,32 @@ where b.starttime >= '2012-09-14'
   and b.starttime < '2012-09-15'
   and ((m.memid = 0 and b.slots * f.guestcost > 30) or (m.memid != 0 and b.slots * f.membercost > 30))
 order by cost desc;
+
+-- How can you output a list of all members, including the individual who recommended them (if any), without using any joins?
+-- Ensure that there are no duplicates in the list, and that each firstname + surname pairing is formatted as a column and ordered.
+select distinct concat(mem.firstname, ' ', mem.surname) member,
+                (select concat(rec.firstname, ' ', rec.surname)
+                 from cd.members rec
+                 where rec.memid = mem.recommendedby)   recommender
+from cd.members mem
+order by member;
+
+
+-- The Produce a list of costly bookings exercise contained some messy logic: we had to calculate the booking cost in both the WHERE clause and the CASE statement.
+
+-- Try to simplify this calculation using subqueries. For reference, the question was:
+-- How can you produce a list of bookings on the day of 2012-09-14 which will cost the member (or guest) more than $30?
+-- Remember that guests have different costs to members (the listed costs are per half-hour 'slot'), and the guest user is always ID 0.
+-- Include in your output the name of the facility, the name of the member formatted as a single column, and the cost. Order by descending cost.
+select member, facility, cost
+from (select concat(m.firstname, ' ', m.surname)                                              member,
+             f.name                                                                           facility,
+             case when m.memid = 0 then f.guestcost * b.slots else f.membercost * b.slots end cost
+      from cd.members m
+               inner join cd.bookings b on m.memid = b.memid
+               inner join cd.facilities f on b.facid = f.facid
+      where b.starttime >= '2012-09-14'
+        and b.starttime < '2012-09-15'
+      order by cost desc) booking
+where cost > 30.0
+order by cost desc;
